9ba9c12da766f605f1e100d929ae30a1f91ec6e9
Leo Laine <leo.laine.1980@gmail.com>
2025-05-21 12:18:49 +0300

    code refactoring

--- server/openai.php
+++ server/openai.php
@@ -2,6 +2,6 @@
 require_once 'vendor/autoload.php';
 use GuzzleHttp\Client;
 
-$OPENAI_API_KEY = 'Your OpenAI API key here';
+$OPENAI_API_KEY = 'YOUR OPENAI API KEY HERE';
 $openai_client = OpenAI::client($OPENAI_API_KEY);
 ?>
