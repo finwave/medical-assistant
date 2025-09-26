a8e63df23257d367941459d3257bb4f32b76f545
Leo Laine <leo.laine.1980@gmail.com>
2025-09-26 20:19:42 +0300

    code refactoring (tab space)

--- server/api.php
+++ server/api.php
@@ -11,25 +11,25 @@ header("Content-Type: application/json; charset=UTF-8");
 $method = $_SERVER['REQUEST_METHOD'];
 
 switch ($method) {
-    case 'GET':
-        if (isset($_GET['input'])) {
-            $input = $_GET['input'];
-            $output = $openai_client->chat()->create([
-                'model' => 'gpt-4o-mini',
-                'messages' => [[
-                    'role' => 'user',
-                    'content' => $input,
-                    ]],
-            ]);
-            $result = $output['choices'][0]['message']['content'];
-            $data = array('result' => $result);
-            echo json_encode($data);
-        } else {
-            echo json_encode("ERROR");
-        }
-        break;
-    default:
-        echo json_encode("ERROR");
-        break;
+  case 'GET':
+    if (isset($_GET['input'])) {
+      $input = $_GET['input'];
+      $output = $openai_client->chat()->create([
+        'model' => 'gpt-4o-mini',
+        'messages' => [[
+          'role' => 'user',
+          'content' => $input,
+          ]],
+        ]);
+      $result = $output['choices'][0]['message']['content'];
+      $data = array('result' => $result);
+      echo json_encode($data);
+    } else {
+      echo json_encode("ERROR");
+    }
+    break;
+  default:
+    echo json_encode("ERROR");
+    break;
 }
 ?>
