362d9ef6ade26798d16cd0856672b84fd288905a
Leo Laine <leo.laine.1980@gmail.com>
2025-09-26 20:34:25 +0300

    multi page / builder source file (chooses what page to show)

---
+++ src/app/builder.tsx
@@ -0,0 +1,38 @@
+"use client"
+
+import { useState, useEffect } from 'react'
+import { Page } from '@/types/enums';
+import HeaderMenu from '@/app/header'
+import Profile from "@/pages/profile"
+import Assistant from "@/pages/assistant"
+
+export default function Builder() {
+  const [mounted, setMounted] = useState(false)
+  const [page, setPage] = useState(Page.Assistant);
+
+  // useEffect only runs on the client, so now we can safely show the UI
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  if (!mounted) {
+    return null;
+  }
+
+  function showPage(page: Page) {
+    return (
+      <>
+        <HeaderMenu setPage={setPage} />
+        {
+          page == Page.Profile ? (
+            <Profile />
+          ) : page == Page.Assistant ? (
+            <Assistant />
+          ) : null
+        }
+      </>
+    )
+  }
+
+  return showPage(page);
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,10 @@
-import ThemeSwitch from "@/components/theme-switch"
-import LanguageSwitch from "@/components/language-switch";
-import InputRequest from "@/components/input-request"
-import {useTranslations} from 'next-intl';
+import Builder from "@/app/builder"
 
 export default function Home() {
-  const t_general = useTranslations('General');
-  const t_input_page = useTranslations('InputPage');
-
   return (
     <div className="bg-gray-100 dark:bg-gray-900 text-black dark:text-gray-50
-      font-[family-name:var(--font-geist-sans)] text-sm lg:text-base min-w-screen min-h-screen
-      flex flex-col items-center justify-center gap-5 lg:gap-8 pt-14 pb-5 lg:pt-20 lg:pb-14">
-      <InputRequest page_title={t_general('page_title')} openai_template={t_input_page('openai_template')} 
-        request_wait={t_input_page('request_wait')} request_error={t_input_page('request_error')} 
-        input_placeholder={t_input_page('input_placeholder')} button_send={t_input_page('button_send')} 
-        clipboard_notification={t_input_page('clipboard_notification')} />
-      <ThemeSwitch />
-      <LanguageSwitch />
+    font-[family-name:var(--font-geist-sans)] text-sm lg:text-base page_area">
+      <Builder />
     </div>
   );
 }
